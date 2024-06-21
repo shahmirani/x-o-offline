f2cc3beaecd8998651ea542cf3b4d97f72466db5
MissS <fshahmir38@gmail.com>
2024-06-22 00:40:55 +0330

    Finish Offline mode

---
+++ app/src/main/java/ir/shahmirani/x_o/GameData.kt
@@ -0,0 +1,14 @@
+package ir.shahmirani.x_o
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+object GameData {
+    private var _gameModel : MutableLiveData<GameModel> = MutableLiveData()
+    var gameModel : LiveData<GameModel> = _gameModel
+
+    fun saveGameModel(model: GameModel){
+        _gameModel.postValue(model)
+
+    }
+}
